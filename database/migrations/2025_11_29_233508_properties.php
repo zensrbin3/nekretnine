da3acadc05ac5433408f7274c2b17cde99f99118
Petar Stankovic <pstankovic237@gmail.com>
2025-11-30 00:45:22 +0100

    2 tabels added for database

---
+++ database/migrations/2025_11_29_233508_properties.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('location');
+            $table->decimal('price', 12, 2);
+            $table->unsignedInteger('size_m2');
+            $table->enum('type', ['house', 'apartment', 'land', 'office'])->default('apartment');
+            $table->enum('status', ['active', 'sold', 'expired'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
