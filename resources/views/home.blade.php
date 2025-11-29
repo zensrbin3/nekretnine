4477e05772b59af9c89dbd4bd9359c864f303a92
Petar Stankovic <pstankovic237@gmail.com>
2025-11-29 23:31:50 +0100

    Started version

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title','Home')
+@section('content')
+    <h1>Cao</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-4">
+    @yield('content')
+</div>
+<script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
