1464129dd8b4ef400c54bda7cdd910b746ba3cda
Petar Stankovic <pstankovic237@gmail.com>
2026-01-12 22:09:36 +0100

    added livewire component to search properties in home.blade

--- resources/views/livewire/search-users.blade.php
+++ resources/views/livewire/search-users.blade.php
@@ -1,3 +1,19 @@
-<div>
+<div class="position-relative w-50 mx-auto">
+    <input
+        type="text"
+        class="form-control"
+        placeholder="Pretraži nekretnine..."
+        wire:model.live="search"
+    >
 
+    @if($search !== '' && $properties->count())
+        <div class="list-group position-absolute w-100 mt-1 shadow">
+            @foreach($properties as $property)
+                <a href="#"
+                   class="list-group-item list-group-item-action">
+                    {{ $property->title }}
+                </a>
+            @endforeach
+        </div>
+    @endif
 </div>
